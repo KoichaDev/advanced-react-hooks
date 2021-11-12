a447e15e9053d59a9f9d7b5c5a49fd09095ca88f
Khoi Hoang <khoii87@gmail.com>
2021-11-13 00:09:12 +0100

    Update: 💄 Title and Author description and image of the site

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,14 +6,70 @@ const Wrapper = styled.div`
   background: linear-gradient(200.44deg, #4316db 13.57%, #9076e7 98.38%);
 `
 
+const TextWrapper = styled.div`
+  position: relative;
+  display: grid;
+  gap: 1.25em;
+  padding-top: 13.75em;
+`
+
+const AuthorWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1em;
+`
+
 const Logo = styled.img`
   width: 60px;
   height: 60px;
 `
 
+const Title = styled.h1`
+  max-width: 500px;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 3.75rem;
+  line-height: 4.5rem;
+  color: #fff;
+  mix-blend-mode: normal;
+  text-shadow: 0px 20px 40px rgba(0, 0, 0, 0.3);
+`
+
+const Caption = styled.p`
+  font-size: 1rem;
+  font-style: normal;
+  line-height: 130%;
+  text-transform: uppercase;
+  color: rgba(255, 255, 255, 0.7);
+`
+
+const Description = styled.p`
+  max-width: 400px;
+  font-style: normal;
+  font-size: 20px;
+  line-height: 140%;
+  color: #fff;
+`
+
+const AuthorImage =styled.img`
+  width: 32px;
+  height: 32px;
+`
+
 const IndexPage = () => (
   <Wrapper>
-    <Logo src="/images/logos/react-logo.svg" alt="icon"/>
+    <TextWrapper>
+      <Logo src="/images/logos/react-logo.svg" alt="icon" />
+      <Title>Build a web app with React Hooks</Title>
+      <Caption>20 sections - 3 hours of videos</Caption>
+      <Description>
+        Learn how we build the new DesignCode site with React Hooks
+      </Description>
+      <AuthorWrapper>
+      <AuthorImage src="/images/avatars/Meng.png" alt="author image"/>
+      <Caption>Taught by Meng To</Caption>
+      </AuthorWrapper>
+    </TextWrapper>
   </Wrapper>
 )
 
