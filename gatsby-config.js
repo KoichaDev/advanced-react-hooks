6200fe280b0579bc04144ed3238ce92565d0a770
Khoi Hoang <khoii87@gmail.com>
2021-11-12 23:17:01 +0100

    Update: ✨ Added TypeScript on gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatbsy-plugin-typescript`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
